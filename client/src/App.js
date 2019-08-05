26863f7f1d993fc9a2994f5f5985160128c93a31
arjun2010.ece <arjun2010.ece@gmail.com>
2019-08-05 16:27:07 +0100

    Devconnector project issue in nodejs

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import { loadUser } from './actions/auth';
+
+import './App.css';
+import setAuthToken from './utils/setAuthToken';
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() =>{
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store = {store} >
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+           <Alert />
+          <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute 
+                exact 
+                path="/create-profile" 
+                component={CreateProfile} 
+              />
+              <PrivateRoute 
+                exact 
+                path="/edit-profile" 
+                component={EditProfile} 
+              />
+          </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ client/src/reducers/profile.js
@@ -0,0 +1,36 @@
+import { 
+    GET_PROFILE, 
+    PROFILE_ERROR,
+    CLEAR_PROFILE
+    } from '../actions/types';
+
+
+const initialState = {
+    profile: null,
+    profiles: [],
+    repos: [],
+    loading: true,
+    error: {}
+}
+
+export default function(state= initialState, action){
+    const { type, payload } = action;
+    switch (type) {
+        case GET_PROFILE:
+            return { 
+                ...state,
+                profile: payload,
+                loading: false
+            };
+        case PROFILE_ERROR:
+            return { 
+                ...state,
+                loading: false,
+                error: payload
+            };
+        case CLEAR_PROFILE:
+            return {...state, loading: false, profile: null, repos: [] };
+        default:
+            return state;
+    }
+}
